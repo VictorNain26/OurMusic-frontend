035819b61bfcc8d38e2677c41b476fbb49a84d4c
Victor <victor.lenain26@gmail.com>
2025-03-01 14:31:14 +0100

    add chromecast initialize in index.html

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,13 @@
-/* global cast chrome */
-import { useEffect } from 'react';
+/* global cast */
 import AzuracastPlayer from './../components/AzuracastPlayer';
 
 const HomePage = () => {
-  useEffect(() => {
-    // Cette fonction est appelée par le SDK Cast lorsque celui-ci est disponible
-    window.__onGCastApiAvailable = (isAvailable) => {
-      if (isAvailable) {
-        const context = cast.framework.CastContext.getInstance();
-        context.setOptions({
-          receiverApplicationId: '4A922B9B', // Remplacez par l'ID de votre receiver personnalisé
-          autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
-        });
-        console.log("SDK Cast initialisé.");
-      }
-    };
-  }, []);
-
   const handleCast = async () => {
     try {
       const context = cast.framework.CastContext.getInstance();
       await context.requestSession();
       console.log("Session de cast établie.");
-      // Vous pouvez ajouter ici un chargement de média vers le receiver si nécessaire
+      // Ici, vous pouvez éventuellement charger le média sur le receiver
     } catch (error) {
       console.error("Erreur lors du lancement du cast :", error);
     }
