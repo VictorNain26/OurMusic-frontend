dca41c7840676f5ad4d6f30ee4035d76054f44ac
Victor <victor.lenain26@gmail.com>
2025-03-06 15:19:17 +0100

    add modal design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ButtonRefreshSpotify from './components/ButtonRefreshSpotify';
-import ProtectedRoute from './components/ProtectedRoute';
-import PublicRoute from './components/PublicRoute';
-import LoginPage from './components/LoginModal';
-import RegisterPage from './components/RegisterModal';
 
 function App() {
   return (
@@ -12,16 +8,11 @@ function App() {
       {/* Page d'accueil publique */}
       <Route path="/" element={<HomePage />} />
 
-      {/* Routes publiques pour login et register */}
-      <Route element={<PublicRoute />}>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
       {/* Route protégée pour accéder à spotify-refresh */}
-      <Route element={<ProtectedRoute />}>
         <Route path="/spotify-refresh" element={<ButtonRefreshSpotify />} />
-      </Route>
 
       {/* Redirection par défaut */}
       <Route path="*" element={<Navigate to="/" replace />} />
