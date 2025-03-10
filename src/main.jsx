3843bfc5915b87f57dff18b256270d38947179b4
Victor <victor.lenain26@gmail.com>
2025-03-10 10:10:05 +0100

    add correctly package

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
