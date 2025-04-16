120a2c5c127973e9ec64be1547dfed2bf355a18c
Victor <victor.lenain26@gmail.com>
2025-04-16 19:00:31 +0200

    update auth.config.js with mailer service

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
@@ -8,7 +9,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: './',
-    plugins: [react()],
+    plugins: [react(),tailwindcss(),],
     server: {
       host: '0.0.0.0',
       port: 5173,
